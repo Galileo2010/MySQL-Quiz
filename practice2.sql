93c8373828dec7f7f31f711797805acc85054641
Galileo2010 <M201672189@hust.edu.cn>
2019-04-09 00:04:22 +0800

    practice1-3 completed!

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,9 @@
 # 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+select CompanyName, name, M.max_salary 
+from Company Com, 
+     Employee Emp, 
+     (select companyId, max(salary) max_salary from Employee group by companyId) M 
+where Emp.companyId = Com.id 
+      and Emp.companyId = M.companyId 
+      and Emp.salary = M.max_salary;
--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,8 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
-
+select distinct(companyName), avgSalary
+from Company, 
+     Employee,
+     (select companyId, avg(salary) avgSalary from Employee group by companyId) A
+where Employee.companyId = A.companyId and Employee.companyId = Company.id;
 
